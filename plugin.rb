ec7bfd4ef8218d0998778774ab26dd0cc0892182
Joe Buhlig <joe@joebuhlig.com>
2019-03-28 10:06:52 -0500

    FEATURE: Base page for installing in place

--- plugin.rb
+++ plugin.rb
@@ -6,3 +6,9 @@
 
 
 register_asset 'stylesheets/procourse-butler.scss'
+add_admin_route 'procourse_butler.title', 'procourse-butler'
+
+#Adding butler admin page
+Discourse::Application.routes.append do
+  get '/admin/plugins/procourse-butler' => 'admin/plugins#index'
+end
