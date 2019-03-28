8b9f98cd2c8fbe15ce6977460edf5709233acb6b
Joe Buhlig <joe@joebuhlig.com>
2019-03-28 08:50:33 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: procourse-butler
+# about: A Discourse plugin for discovering and installing other Discourse plugins.
+# version: 0.1
+# authors: ProCourse
+# url: https://github.com/procourse/procourse-butler
+#
+#
